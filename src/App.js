97718b6d775eef6d2cdb4e5273635d7d6b750914
Jim Vang <jimvang@Jims-MacBook-Air.local>
2023-12-27 00:44:08 -0600

    updated json data and fetch

--- src/App.js
+++ src/App.js
@@ -9,19 +9,33 @@ import { useState, useEffect } from 'react'
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([])
+  const [archers, setArchers] = useState([])
 
   useEffect(() => {
     const getTasks = async () => {
       const tasksFromServer = await fetchTasks ()
       setTasks(tasksFromServer)
     }
+    const getArchers = async () => {
+      const archersFromServer = await fetchArchers ()
+      setArchers(archersFromServer)
+    }
     getTasks()
+    getArchers()
   }, [])
 
   // Fetch Tasks
   const fetchTasks = async () => {
     const res = await fetch('http://localhost:5000/tasks')
     const data = await res.json()
+    return data
+  }
+
+  //Fetch Archers
+  const fetchArchers = async () => {
+    const res = await fetch('http://localhost:5000/archers')
+    const data = await res.json()
+    console.log('archers data', data)
     return data
   }
 
